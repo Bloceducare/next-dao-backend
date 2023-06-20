2faa70532193845f7aea2633cfe7764a61fc6ea4
okeken <okekehinde@gmail.com>
2023-06-20 14:21:40 +0100

    fix:deploy

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -3,15 +3,11 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import User from "@models/user"
 import Transaction from "@models/transaction"
 import { getUsers , getUser} from 'server/services/user'
-import { createRouter } from 'next-connect';
 import { getTransaction } from 'server/services/transaction';
 import connectDB from 'config/db';
 
 
 connectDB();
-
-// const router = createRouter<NextApiRequest, NextApiResponse>();
-
 
 
 export default async function handler(
@@ -106,103 +102,4 @@ export default async function handler(
     
   }
 
-  
-// router.get('/', async (req: NextApiRequest, res: NextApiResponse) => {
-
-// });
-
-// ("/", async (req: NextApiRequest, res: NextApiResponse)=>{
-//     const { walletAddress, txnId } = req.body;
-//     const amount = Number(req.body.amount);
-//     const currentDate = Date.now();
-//     if (!walletAddress || !txnId) {
-//       return res.status(403).json({
-//         success: false,
-//         message: "All fields are required",
-//       });
-//     }
-
-    
-// try {
-
-//     // Find the user with the specified wallet address
-//     const findUser = getUser(walletAddress)
-//     const findTx =  getTransaction(txnId)
-//     let [user, tx] = await Promise.all([findUser, findTx]);
-//     if (!tx) {
-//       Transaction.create({
-//         user: walletAddress,
-//         txnId,
-//         amount,
-//         data: currentDate,
-//       });
-//     }
-
-//     if (user) {
-//       let updateUser = user;
-//       if (!tx) {
-//         updateUser = await User.findOneAndUpdate(
-//           { walletAddress },
-//           {
-//             amount: user.amount + amount,
-//             $push: {
-//               transactionId: {
-//                 txnId,
-//                 amount,
-//                 date: currentDate,
-//               },
-//             },
-//           }
-//         );
-//       }
-
-//       return res.status(201).send({
-//         success: true,
-//         message: "Amount Updated Successfully",
-//         data: updateUser._doc,
-//       });
-//     } else {
-//       const newUser = await User.create({
-//         ...req.body,
-//         transactionId: [
-//           {
-//             txnId: req.body.txnId,
-//             amount,
-//             date: currentDate,
-//           },
-//         ],
-//       });
-
-//       return res.status(201).send({
-//         success: true,
-//         message: "User Details Added Successfully",
-//         data: newUser._doc,
-//       });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(400).send({
-//       success: false,
-//       message: error,
-//     });
-//   }
-
-
-
-// })
-
-// export default router.handler({
-//     // @ts-ignore
-//     onError: (err, req, res, next) => {
-//       console.error(err.stack);
-//     return  res.status(500).json({
-//         error:"Server error"
-//       })
-//     },
-//     onNoMatch: (req, res) => {
-//      return res.status(404).json({
-//         error:"Method not Allowed"
-//      });
-//     },
-//   });
-  
+ 
